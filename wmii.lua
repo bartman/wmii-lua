30c3bdbc09733571ceade42252d42228c4991403
Bart Trojanowski <bart@jukie.net>
2007-08-31 17:53:49 -0400

    added a simple event iterator

--- wmii.lua
+++ wmii.lua
@@ -10,9 +10,12 @@
 local base = _G
 local io = require("io")
 local os = require("os")
+local posix = require("posix")
+local string = require("string")
 
 module("wmii")
 
+-- ------------------------------------------------------------------------
 -- TODO I would like to be able to return an interator
 function ls (dir, fmt)
         local tmpfile = os.tmpname()
@@ -30,11 +33,51 @@ function ls (dir, fmt)
         return data
 end
 
--- TODO I would like to return a line iterator
+-- ------------------------------------------------------------------------
+-- read all contents of a wmii virtual file
 function read (file)
         local tmpfile = os.tmpname()
 
         os.execute ("wmiir read " .. file .. " > " .. tmpfile)
+
+        local fh = io.open (tmpfile, "rb")
+        os.remove (tmpfile)
+
+        local data = fh:read("*a")      -- read all
+        io.close (fh)
+
+        return data
+end
+
+-- ------------------------------------------------------------------------
+-- return an iterator which walks all the lines in the file
+--
+-- example:
+--     for event in wmii.iread("/event")
+--         ...
+--     end
+function iread (file)
+        local tmpfile = os.tmpname()
+        os.remove (tmpfile)
+
+        io.write ("-- tmpname " .. tmpfile .. "\n")
+
+        local rc = posix.mkfifo (tmpfile)
+        io.write ("-- mkfifo " .. rc .. "\n")
+
+        rc = posix.fork ()
+        if rc < 0 then
+                io.write ("-- fork failed " .. rc .. "\n")
+                return function ()
+                        return nil
+                end
+        end
+        if rc == 0 then -- child
+                os.execute ("wmiir read " .. file .. " > " .. tmpfile)
+                posix.exec ("/usr/bin/env", "cat", "/dev/null")
+        end
+
+        -- parent
 
         local fh = io.open (tmpfile, "rb")
         os.remove (tmpfile)
@@ -49,6 +92,18 @@ function read (file)
         end
 end
 
+-- ------------------------------------------------------------------------
+-- returns an events iterator
+function ievents ()
+        local it = iread("/event")
+
+        return function ()
+                local line = it()
+                return string.match(line, "(%S+)%s(.+)")
+        end
+end
+
+-- ------------------------------------------------------------------------
 -- write a value to a wmii virtual file system
 function write (file, value)
         os.execute ("echo -n '" .. value .. "' | wmiir write " .. file)
--- wmiirc.lua
+++ wmiirc.lua
@@ -4,17 +4,23 @@ require "wmii"
 
 wmii.write ("/lbar/1", '#FF0000 #00FF00 #0000FF xxx')
 
-foo = wmii.ls ("/lbar")
+foo = wmii.ls ("/")
 io.write ("ls ::\n" .. foo .. "\n")
 
 foo = wmii.ls ("/lbar", "-l")
 io.write ("ls -l ::\n" .. foo .. "\n")
 
 foo = wmii.read ("/lbar/1")
-io.write ("read /lbar/1 ::\n")
+io.write ("read /lbar/1 ::\n" .. foo .. "\n\n")
 
-for x in foo do
-        io.write (x .. "\n")
+
+io.write ("read some events...\n")
+--[[
+for x in wmii.iread("/event") do
+        io.write ("ev: " .. x .. "\n")
 end
-
+]]--
+for x,y in wmii.ievents() do
+        io.write ("ev: " .. x .. " - " .. y .. "\n")
+end
 
