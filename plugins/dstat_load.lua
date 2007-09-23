645fefdfb94993a744f1abe3f6619d1975d8f063
Bart Trojanowski <bart@jukie.net>
2007-09-23 15:35:34 -0400

    colourize the load output

--- plugins/dstat_load.lua
+++ plugins/dstat_load.lua
@@ -7,20 +7,50 @@
 --
 local wmii = require("wmii")
 local os = require("os")
+local math = require("math")
+local string = require("string")
 local type = type
+local tonumber = tonumber
+local tostring = tostring
 
 module("dstat_load")
 api_version=0.1
 
-widget = wmii.widget:new ("800_dstat_load")
+local palette = { "#888888",
+                  "#999988",
+                  "#AAAA88",
+                  "#BBBB88",
+
+                  "#CCCC88",
+                  "#CCBB88",
+                  "#CCAA88",
+
+                  "#DD9988",
+                  "#EE8888",
+                  "#FF4444",
+          }
+
+local widget = wmii.widget:new ("800_dstat_load")
 wmii.add_exec ("TERM=vt100 dstat --load --nocolor --noheaders --noupdate 1",
                 function (line)
-                        if not (type(line) == "string") then
+                        if type(line) ~= "string" then
                                 return
                         end
 
                         local line = line:gsub ("%W%W+", " ")
-                        if line:len() > 3 then
-                                widget:show (line)
+                        if line:len() < 5 then
+                                return
                         end
+
+                        local tmp = line:match("([%d.]+)%D")
+                        local current = tonumber(tmp)
+
+                        local colors = nil
+                        if type(current) == "number" then
+                                local index = math.min (math.floor(current * (#palette-1)) + 1, #palette)
+                                local normal = wmii.get_ctl("normcolors")
+                                colors = string.gsub(normal, "^%S+", palette[index], 1)
+                        end
+
+                        widget:show (line, colors)
                 end)
